e3dddf3886cdf1f3accbdad9f11800378404afce
e232352 <pawan.peddi@evry.com>
2021-04-03 11:14:13 +0530

    Modifications in files

---
+++ src/client/assets/scripts/script.js
@@ -0,0 +1,22 @@
+
+let step_boxClass = document.getElementsByClassName("step-box");
+
+for(let i=0; i<step_boxClass.length; i++) {
+    step_boxClass[i].addEventListener('click', function(e) {
+        console.log(this)
+        let step_contentClass = document.getElementsByClassName("stepContent");
+        let currentActiveStepContent = document.getElementsByClassName("activeContent");
+        let currentActiveStepBox = document.getElementsByClassName("currentBox");
+
+        currentActiveStepContent[0].className = currentActiveStepContent[0].className.replace(" activeContent", "");
+        currentActiveStepBox[0].className = currentActiveStepBox[0].className.replace(" currentBox", "");
+
+        step_contentClass[i].className += " activeContent";
+        step_boxClass[i].className += " currentBox";
+
+        //this.classList.toggle('active-content');
+        console.log(this.className);
+        e.preventDefault();
+    });
+    
+}
